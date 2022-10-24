a18e308a6d6be69a68b575e4ebc7abdf6b472882
arnoldaguila04 <arnoldaguila04@gmail.com>
2022-10-24 13:42:54 -0400

    Created a get_word function that reads a text file. Then is returns a random word from the text file.

--- hangman.py
+++ hangman.py
@@ -1,4 +1,19 @@
 from random import randint
 
 def get_word(filename):
+    '''
+    This function gets and returns a random word from a word bank.
+    :param filename: word_bank.txt is a list of words.
+    :return: random word from the word_bank.txt file.
+    '''
     word_file = open(filename, 'r')
+    word_file_readline = word_file.readline()
+    return word_file_readline[randint(0, len(word_file_readline) - 1)].strip()
+
+def console_game(word):
+    guesses = []
+
+
+
+if __name__ == '__main__':
+    pass
