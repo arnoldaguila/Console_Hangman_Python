696f281cdc7a6dc01f7ff802276e82033ef9459a
arnoldaguila04 <arnoldaguila04@gmail.com>
2022-10-25 21:44:47 -0400

    Created a with block that reads and closes the read file stream, then returns a random word. Number of guesses was created for how many body parts there are for a stick figure. Now getting the user input.

--- hangman.py
+++ hangman.py
@@ -2,18 +2,21 @@ from random import randint
 
 def get_word(filename):
     '''
-    This function gets and returns a random word from a word bank.
+    This function gets and returns a random word from a text file that contain a lot of words.
     :param filename: word_bank.txt is a list of words.
     :return: random word from the word_bank.txt file.
     '''
-    word_file = open(filename, 'r')
-    word_file_readline = word_file.readline()
-    return word_file_readline[randint(0, len(word_file_readline) - 1)].strip()
+    with open(filename, "r") as word_file:
+        word_file_lines = word_file.readlines()
+        return word_file_lines[randint(0, len(word_file_lines) - 1)].strip()
 
 def console_game(word):
     guesses = []
+    number_of_guesses = 6 # six parts of the body. Head, body, left arm, right arm, left leg, right leg.
+    user_guess = input("Enter a letter: ")
+
 
 
 
 if __name__ == '__main__':
-    pass
+    print(get_word("word_bank.txt"))
