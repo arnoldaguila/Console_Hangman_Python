6e6f002ad2e4ff9d6488de8e866556aaed30886d
arnoldaguila04 <arnoldaguila04@gmail.com>
2022-10-27 15:11:03 -0400

    Trying to figure out how to create blank lines the size of the random word. Then updating the blanklines for every guess.

--- hangman.py
+++ hangman.py
@@ -4,19 +4,36 @@ def get_word(filename):
     '''
     This function gets and returns a random word from a text file that contain a lot of words.
     :param filename: word_bank.txt is a list of words.
-    :return: random word from the word_bank.txt file.
+    :return word_file_lines: String, random word from the word_bank.txt file.
     '''
     with open(filename, "r") as word_file:
         word_file_lines = word_file.readlines()
         return word_file_lines[randint(0, len(word_file_lines) - 1)].strip()
 
 def console_game(word):
-    guesses = []
+    '''
+    This function is the console game for hangman.
+    :param word: String, a word that was pulled form get_word.
+    :return:
+    '''
     number_of_guesses = 6 # six parts of the body. Head, body, left arm, right arm, left leg, right leg.
-    user_guess = input("Enter a letter: ")
+    blank_lines = "_" * len(word) # creating blank lines for user experience.
+    blank_lines_list = list(blank_lines)
+    while number_of_guesses != 0:
+        guesses = []
+        user_input = input("Enter a letter: ")
+        guesses.append(user_input)
+        for i in guesses:
+            for j in range(len(word)):
+                if word[j] == i:
+                    blank_lines[j] = i
+            return blank_lines
+
 
 
 
 
 if __name__ == '__main__':
-    print(get_word("word_bank.txt"))
+    word = get_word("word_bank.txt") # getting word
+    print(word)
+    print(console_game(word)) # playing console hangman
