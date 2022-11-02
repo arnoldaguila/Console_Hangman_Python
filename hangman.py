a6132f6efec694dac93109a18e446b95ef934235
arnoldaguila04 <arnoldaguila04@gmail.com>
2022-11-01 20:22:51 -0400

    Created a guessed_letter bank so that if the user inputs the same letter twice. They will get a strike on the number_of_guesses counter.

--- hangman.py
+++ hangman.py
@@ -22,46 +22,61 @@ def console_game(word):
     # creating blank lines for user experience.
     blank_lines = "_" * len(word)
     blank_lines_list = list(blank_lines)
+    # guess bank so that user can't input the same letter twice
+    guess_bank = []
     # creating a win/loss message
     win = False
     loss = False
+
     while number_of_guesses != 0:
         user_input = input("Enter a letter: ") # userInput
+        print()
         if len(user_input) > 1:
             print("Invalid input, please input 1 letter.")
         else:
-            # if letter is not in the word
-            if((user_input in word) == False):
+            # if letter has been guessed before.
+            if ((user_input in guess_bank) == True):
+                print("You guess the letter before dude!")
                 number_of_guesses = number_of_guesses - 1
-                if number_of_guesses == 0:
-                    loss = True
-                    break
-                else:
-                    print("".join(blank_lines_list))
-                    print("Guesses Remaining: {} \n".format(number_of_guesses))
+                print("".join(blank_lines_list))
+                print("Guesses Remaining: {} \n".format(number_of_guesses))
             else:
-                for letter in user_input:
-                    for i in range(len(word)):
-                        if word[i] == letter:
-                            blank_lines_list[i] = letter
-                if ("".join(blank_lines_list) == word):
-                    win = True
-                    print("".join(blank_lines_list))
-                    break
+                # if letter is not in the word
+                if((user_input in word) == False):
+                    guess_bank = user_input
+                    number_of_guesses = number_of_guesses - 1
+                    # checking to see if number_of_guess is 0 for the loss message.
+                    if number_of_guesses == 0:
+                        loss = True
+                        break
+                    else:
+                        print("".join(blank_lines_list))
+                        print("Guesses Remaining: {} \n".format(number_of_guesses))
                 else:
-                    print("".join(blank_lines_list))
-                    print("Guesses Remaining: {} \n".format(number_of_guesses))
+                    guess_bank = user_input
+                    for letter in user_input:
+                        for i in range(len(word)):
+                            if word[i] == letter:
+                                blank_lines_list[i] = letter
+                    if ("".join(blank_lines_list) == word):
+                        win = True
+                        print("".join(blank_lines_list))
+                        break
+                    else:
+                        print("".join(blank_lines_list))
+                        print("Guesses Remaining: {} \n".format(number_of_guesses))
 
     #loss message
     if loss:
         print("You lost.")
         print("The word was {}".format(word))
 
+    #win message
     if win:
         print("You won!!")
 
 
-# main method
+# main function?
 if __name__ == '__main__':
     word = get_word("word_bank.txt") # getting word
     print(word)
