229697b67bc55b94650727685d6a431e64f73356
Arnold Aguila <arnoldaguila04@gmail.com>
2023-05-30 23:44:08 -0700

    -Added doc comments on the __init__ method. -Added doc comments on the get_word method. -Added return None on the get_word method. -Added doc comments on the print_hangman method. -Added the static method game_message. -Added the check_user_input method. -Added the run method.

--- ConsoleHangman.py
+++ ConsoleHangman.py
@@ -3,10 +3,19 @@ from random import randint
 
 class Hangman:
     def __init__(self, txt_file):
+        """
+        Initializes the Hangman class.
+        """
         self.file = txt_file
         self.guesses_remaining = 6
 
     def get_word(self):
+        """
+        Chooses and returns a random word from the instance file.
+
+        Returns:
+            str: A random word from file.
+        """
         # checking to see if you have a file.
         try:
             with open(self.file, "r") as word_file:
@@ -14,8 +23,12 @@ class Hangman:
                 return word_bank[randint(0, len(word_bank) - 1)].strip()
         except FileNotFoundError:
             print("Please create or download a text file that has a list of words.")
+            return None
 
     def print_hangman(self):
+        """
+        Displays the current state of the game.
+        """
         if self.guesses_remaining == 6:
             print("_________")
             print("|       |")
@@ -67,11 +80,43 @@ class Hangman:
             print("|      / \\")
             print("|")
 
+    @staticmethod
+    def game_message():
+        """
+        Print out a welcome and controls message. The control message will inform the user of the game features.
+        """
+        print()
+        print("Hello, Welcome to Arnold's console hangman game.")
+        print("You can end the game at anytime by typing in 'end', when asked to input a letter.")
+        print("You can also restart at anytime by typing in restart.")
+
+    def check_user_input(self, user_input):
+        """
+        Checks and cleans the argument by removing all spaces and setting the str to upper. This method also checks
+        if the argument is valid i.e. the argument is a 1 letter.
+
+        Parameters:
+            user_input (str): The user input.
+        """
+        cleaned_input = user_input.replace(" ", "")
+        cleaned_input = cleaned_input.upper()
+        if cleaned_input is "END":
+            pass
+        elif cleaned_input is "RESTART":
+            pass
+        elif len(cleaned_input) > 1:
+            pass
+
+    def run(self):
+
+        self.game_message()
+        while True:
+            user_input = input()
+
 
 def main():
     file = "word_bank.txt"
     x = Hangman(file)
-    print(x.get_word())
 
 
 if __name__ == '__main__':
