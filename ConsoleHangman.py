fd52a9ffffc4edba5a8b5d4bdadfa9f300bfd2d9
Arnold Aguila <arnoldaguila04@gmail.com>
2023-06-18 20:05:27 -0700

    - Edited the static method display. - Added the staticmethod tag and edited the print_hangman method. - Added new lines and edited lines of code in the run method. - Added x.run() command in the main function.

--- ConsoleHangman.py
+++ ConsoleHangman.py
@@ -7,7 +7,23 @@ class Hangman:
         Initializes the Hangman class.
         """
         self.file = txt_file
-        self.guesses_remaining = 6
+
+    @staticmethod
+    def display(the_word, target_index=None):
+        """
+        Creates underscores for display purposes.
+
+        Returns:
+            str: A string of underscores.
+        """
+        display = ""
+        for i in range(0, len(the_word)):
+            if target_index is not None and target_index == i:
+                display += the_word[i]
+            else:
+                display += "_"
+
+        return display
 
     def get_word(self):
         """
@@ -25,11 +41,12 @@ class Hangman:
             print("Please create or download a text file that has a list of words.")
             return None
 
-    def print_hangman(self):
+    @staticmethod
+    def print_hangman(guesses_remaining):
         """
         Displays the current state of the game.
         """
-        if self.guesses_remaining == 6:
+        if guesses_remaining == 6:
             print("_________")
             print("|       |")
             print("|")
@@ -37,42 +54,42 @@ class Hangman:
             print("|")
             print("|")
             print("|")
-        elif self.guesses_remaining == 5:
-            print("_________")
-            print("|       |")
-            print("|       O")
-            print("|")
-            print("|")
-            print("|")
-        elif self.guesses_remaining == 4:
+        elif guesses_remaining == 5:
             print("_________")
             print("|       |")
             print("|       O")
+            print("|")
+            print("|")
+            print("|")
+        elif guesses_remaining == 4:
+            print("_________")
+            print("|       |")
+            print("|       O")
             print("|       |")
             print("|")
             print("|")
-        elif self.guesses_remaining == 3:
+        elif guesses_remaining == 3:
             print("_________")
             print("|       |")
             print("|       O")
             print("|      /|")
             print("|")
             print("|")
-        elif self.guesses_remaining == 2:
+        elif guesses_remaining == 2:
             print("_________")
             print("|       |")
             print("|       O")
             print("|      /|\\")
             print("|")
             print("|")
-        elif self.guesses_remaining == 1:
+        elif guesses_remaining == 1:
             print("_________")
             print("|       |")
             print("|       O")
             print("|      /|\\")
             print("|        \\")
             print("|")
-        elif self.guesses_remaining == 0:
+        elif guesses_remaining == 0:
             print("_________")
             print("|       |")
             print("|       O")
@@ -90,64 +107,75 @@ class Hangman:
         print("You can end the game at anytime by typing in 'end', when asked to input a letter.")
         print("You can also restart at anytime by typing in restart.")
 
-    def check_user_input(self, user_input):
-        """
-        Checks and cleans the argument by removing all spaces and setting the str to upper. This method also checks
-        if the argument is valid i.e. the argument is a 1 letter.
-
-        Args:
-            user_input (str): The user input.
-
-        Returns:
-            None: None is used if user_input is END
-            int: 1 is used if user_input is RESTART
-            int: 2 is used if user_input not a letter or user_input has the length of greater than 1.
-        """
-        cleaned_input = user_input.replace(" ", "")
-        cleaned_input = cleaned_input.upper()
-        is_it_a_char = ord(cleaned_input)
-        if cleaned_input == "END":
-            return None
-        elif cleaned_input == "RESTART":
-            return 1
-        elif len(cleaned_input) > 1:
-            return 2
-        elif is_it_a_char < 65 or is_it_a_char > 90:
-            return 2
-        elif len(cleaned_input) == 1:
-            return cleaned_input
-
     def run(self):
         """
         Method that runs the game.
         """
-        # Display game message.
-        self.game_message()
-        # Get a random word from the word bank file.
-        the_word = self.get_word()
-        # Game loop.
+        # Game loop so that user can restart.
         while True:
-            # Get user input.
-            user_input = input()
-            # Check user input is valid.
-            cleaned_input = self.check_user_input(user_input)
-            if cleaned_input is None:
-                break
-            elif cleaned_input == 1:
-                restart = True
-                break
-            elif cleaned_input == 2:
-                print("Error, invalid input.")
+            # Get a random word from the word bank file.
+            the_word = self.get_word()
+            the_word_but_upper = the_word.upper()
+
+            guesses_remaining = 6
+
+            # Display game message.
+            self.game_message()
+
+            # Creating underscores for game display purpose.
+            blank_lines = self.display(the_word)
+
+            # Game loop.
+            while True:
+                self.print_hangman(guesses_remaining)
+                print(blank_lines)
+                print(f"The word: {the_word}")  # This is temp for testing purpose.
+
+                # Get user input.
+                user_input = input("Enter an letter: ")
                 print()
-                continue
-            elif isinstance(cleaned_input, str):
-                if cleaned_input in the_word:
-                    pass
+
+                # Remove spaces from input, change the String to upper case, making sure the input is a char.
+                cleaned_input = user_input.replace(" ", "")
+                cleaned_input = cleaned_input.upper()
+                char_len = ord(cleaned_input[0])
+                # Getting the length to see if it is more than 1 letter.
+                cleaned_input_length = len(cleaned_input)
+
+                if cleaned_input == "END":
+                    print("Ending game.")
+                    print()
+                    return
+                elif cleaned_input == "RESTART":
+                    print("Restarting Game.")
+                    print()
+                    break
+                elif cleaned_input_length > 1:
+                    print("Not 1 letter.")
+                    print()
+                elif cleaned_input_length < 1:
+                    print("Enter a letter.")
+                    print()
+                elif char_len < 65 or char_len > 90:
+                    print("Error: Not a letter.")
+                    print()
+                elif cleaned_input_length == 1:
+                    if cleaned_input in the_word_but_upper:
+                        location_of_letter = the_word_but_upper.index(cleaned_input)
+                        blank_lines = self.display(the_word, location_of_letter)
+                        print(blank_lines)
+                    else:
+                        guesses_remaining -= 1
+
+                else:
+                    print("Error: Invalid input.")
+                    print()
 
 
 def main():
     file = "word_bank.txt"
     x = Hangman(file)
+    x.run()
 
 
 if __name__ == '__main__':
